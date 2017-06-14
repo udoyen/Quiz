99c0957295e3010ad5b604414e65abc1fbef8cb6
george udosen <datameshprojects@gmail.com>
2017-06-14 12:48:23 +0100

    all done except submit button

--- app/src/main/java/com/example/android/quiz/MainActivity.java
+++ app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.android.quiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,7 +16,11 @@ public class MainActivity extends AppCompatActivity {
 
     boolean name = false; // Use this to check if user has entered their name
 
-
+    boolean q1lock = false;
+    boolean q2lock = false;
+    boolean q5lock = false;
+    boolean checkLock = false;
+    boolean textAnswerLock = false;
 
 
     @Override
@@ -25,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        checkCheckBoxAnswer();
+        //checkCheckBoxAnswer();
 
         EditText userName = (EditText) findViewById(R.id.nameText);
 
@@ -50,7 +53,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         EditText q3_TextBox = (EditText) findViewById(R.id.q3_answer);
+
 
         final String rightAnswer = getString(R.string.rightAnswer);
 
@@ -77,59 +82,39 @@ public class MainActivity extends AppCompatActivity {
                 // to "String" so we compare it to the "rightAnswer"
                 String userAnswer = String.valueOf(s);
 
+
                 if (s.length() > 0) { //has the user enter a value, if yes go into the if loop
 
                     if (userAnswer.equals(rightAnswer)) { // Is it the right answer, if so enter ans add 20 points
 
-                        score = score + 20;
+                        if (!textAnswerLock) {
 
-                    }
+                            score += 20;
+                            textAnswerLock = true;
+                        }
 
 
-                }
-
-
-            }
-        });
-    }
-
-    /**
-     *
-     */
-    private void checkCheckBoxAnswer() {
-
-        //Get the ids of all the right checkbox
-        //correct answers
-        final CheckBox scrollViewCheckbox = (CheckBox) findViewById(R.id.q4_option4);
-        final CheckBox checkboxViewCheckbox = (CheckBox) findViewById(R.id.q4_option3);
-        final CheckBox textViewCheckbox = (CheckBox) findViewById(R.id.q4_option1);
-        final CheckBox textClassViewCheckbox = (CheckBox) findViewById(R.id.q4_option2);
-
-        // Use if statement to maek sure
-        // all are checked before adding 20 points to the
-        // score variable
-        textViewCheckbox.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (textViewCheckbox.isChecked() && !(textClassViewCheckbox.isChecked())) {
-                    if (scrollViewCheckbox.isChecked()) {
-                        if (checkboxViewCheckbox.isChecked()) {
-                            score = score + 20;
+                    } else {
+                        if (textAnswerLock) {
+                            score -= 20;
+                            textAnswerLock = false;
                         }
                     }
+
                 }
 
+
             }
+
+
         });
     }
+
 
     /**
      * @param view
      */
     public void q1_OnRadioButtonClicked(View view) {
-
-        boolean checker = false;
 
         // Is the button now checked
         boolean checked = ((RadioButton) view).isChecked();
@@ -139,13 +124,16 @@ public class MainActivity extends AppCompatActivity {
             case R.id.q1_option1:
                 if (checked)
                     score = score + 20;
-                    //checker = true;
+                q1lock = true;
                 break;
             case R.id.q1_option2:
             case R.id.q1_option3:
-                if(checked){
-                    if(score > 0){
-                        score -= 20;
+                if (checked) {
+                    if (q1lock) {
+                        if (score > 0) {
+                            score -= 20;
+                            q1lock = false;
+                        }
                     }
 
                 }
@@ -160,20 +148,30 @@ public class MainActivity extends AppCompatActivity {
      */
     public void q2_OnRadioButtonClicked(View view) {
 
-        // create a group variable using group id
-        RadioGroup q2Button = (RadioGroup) findViewById(R.id.radio_q2);
+        // Is the button now checked
+        boolean checked = ((RadioButton) view).isChecked();
 
-        //Is the button now clicked, then which member of the group
-        // was clicked
-        int checked = q2Button.getCheckedRadioButtonId();
+        // Check which radio button is clicked
+        switch (view.getId()) {
+            case R.id.q2_option2:
+                if (checked)
+                    score = score + 20;
+                q2lock = true;
+                break;
+            case R.id.q2_option1:
+            case R.id.q2_option3:
+                if (checked) {
+                    if (q2lock) {
+                        if (score > 0) {
+                            score -= 20;
+                            q2lock = false;
+                        }
+                    }
 
-        // Use the group is to identify if the clicked radio button
-        // was for the correct answer
-        RadioButton checkedRadioButton = (RadioButton) findViewById(checked);
+                }
 
-        // if the right was selected add 20 points to the score variable
-        if (checkedRadioButton.isChecked()) {
-            score = score + 20;
+            default:
+                break;
         }
     }
 
@@ -181,8 +179,6 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void q5_OnRadioButtonClicked(View view) {
-
-        boolean checker = false;
 
         // Is the button now checked
         boolean checked = ((RadioButton) view).isChecked();
@@ -192,41 +188,73 @@ public class MainActivity extends AppCompatActivity {
             case R.id.q5_option3:
                 if (checked)
                     score = score + 20;
-                    //checker = true;
+                q5lock = true;
                 break;
-            case R.id.q1_option2:
-            case R.id.q1_option3:
-                if(checked){
-
-                    score = score - 20;
+            case R.id.q5_option2:
+            case R.id.q5_option1:
+                if (checked) {
+                    if (q5lock) {
+                        if (score > 0) {
+                            score -= 20;
+                            q5lock = false;
+                        }
+                    }
 
                 }
+
             default:
                 break;
         }
     }
 
     /**
-     *
      * @param view
      */
     public void subtmitButtonClicked(View view) {
 
-        EditText username = (EditText)findViewById(R.id.nameText);
+        EditText username = (EditText) findViewById(R.id.nameText);
 
         // get the value of the user's name and "cast" to primitive type "String"
         String theUser = (username.getText()).toString();
 
         //if(name){ // if user entered a name print name and score of user
 
-            Toast.makeText(MainActivity.this, theUser + " Your total score was " + score, Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, theUser + " Your total score was " + score, Toast.LENGTH_LONG).show();
 
         //}else { // If no user name was entered then notify user and wait for their respond
 
-            //Toast.makeText(MainActivity.this, "Please kindly enter a name at the top of the app", Toast.LENGTH_LONG).show();
+        //Toast.makeText(MainActivity.this, "Please kindly enter a name at the top of the app", Toast.LENGTH_LONG).show();
 
-       // }
+        // }
 
 
     }
+
+    public void checkBoxClicked(View view) {
+
+        //Get the ids of all the right checkbox
+        //correct answers
+        final CheckBox scrollViewCheckbox = (CheckBox) findViewById(R.id.q4_option4);
+        final CheckBox checkboxViewCheckbox = (CheckBox) findViewById(R.id.q4_option3);
+        final CheckBox textViewCheckbox = (CheckBox) findViewById(R.id.q4_option1);
+        final CheckBox textClassViewCheckbox = (CheckBox) findViewById(R.id.q4_option2);
+
+
+        if (scrollViewCheckbox.isChecked() && checkboxViewCheckbox.isChecked() && textViewCheckbox.isChecked() && !textClassViewCheckbox.isChecked()) {
+
+            score += 20;
+            checkLock = true;
+
+        } else {
+            if (checkLock) {
+                if (score > 0) {
+                    score -= 20;
+                    checkLock = false;
+                }
+            }
+
+        }
+    }
+
+
 }
